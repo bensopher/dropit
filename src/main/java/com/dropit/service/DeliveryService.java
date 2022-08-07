c1c726a3db3b28e6520d7dd5dfeaeecace232fb3
Ben Sopher <bensopher1708@gmail.com>
2022-08-07 22:56:19 +0300

    Adding business logic to deliveries api call

--- src/main/java/com/dropit/service/DeliveryService.java
+++ src/main/java/com/dropit/service/DeliveryService.java
@@ -7,13 +7,19 @@ import com.dropit.model.DeliveryRequest;
 import com.dropit.model.Timeslot;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class DeliveryService {
     private static AtomicLong deliveryId = new AtomicLong(1);
+
+    private final int MAX_DELIVERY_PER_TIMESLOT = 2;
+    private final int MAX_DELIVERY_PER_DAY = 10;
     private TimeslotService timeslotService;
+
+    private List<Delivery> allDeliveries;
 
     public DeliveryService(TimeslotService timeslotService) {
         this.timeslotService = timeslotService;
@@ -21,11 +27,28 @@ public class DeliveryService {
 
     public Delivery bookDelivery(DeliveryRequest deliveryRequest) {
         List<Timeslot> availableTimeslots = timeslotService.getAvailableTimeslots();
-        for(Timeslot t : availableTimeslots) {
+        for (Timeslot t : availableTimeslots) {
             if (t.getId().equals(deliveryRequest.timeslotId())) {
-                return new Delivery(deliveryId.getAndIncrement(),DeliveryStatus.WAITING_FOR_DELIVERY, TimeslotDTO.toDTO(t));
+                if (allDeliveries == null) {
+                    allDeliveries = new ArrayList<>();
+                }
+                if (allDeliveries.size() == MAX_DELIVERY_PER_DAY) {
+                    throw new RuntimeException("Day has reached maximum deliveries amount!");
+                }
+                List<Delivery> deliveriesWithTimeslotId = allDeliveries.stream().filter(delivery -> delivery.getTimeslot().getId() == t.getId()).toList();
+                if (deliveriesWithTimeslotId.size() == MAX_DELIVERY_PER_TIMESLOT) {
+                    throw new RuntimeException("Timeslot already has 2 deliveries!");
+                } else {
+                    Delivery newDelivery = new Delivery(deliveryId.getAndIncrement(), DeliveryStatus.WAITING_FOR_DELIVERY, TimeslotDTO.toDTO(t));
+                    allDeliveries.add(newDelivery);
+                    return newDelivery;
+                }
             }
         }
         return new Delivery();
+    }
+
+    public List<Delivery> getAllDeliveries() {
+        return this.allDeliveries;
     }
 }
